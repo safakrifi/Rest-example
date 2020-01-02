4df11a464fedf9852b4b3e6c76a354d51c72a7c9
skrifi <skrifi @FRL00030>
2020-01-02 14:50:53 +0100

    Added and updated exception handlers

--- rest-example/src/main/java/com/example/springbootrestserver/exception/CustomRestExceptionHandler.java
+++ rest-example/src/main/java/com/example/springbootrestserver/exception/CustomRestExceptionHandler.java
@@ -7,11 +7,14 @@ import java.util.List;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
@@ -24,18 +27,30 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.example.springbootrestserver.controller.UserController;
 
 @RestController
 @ControllerAdvice
 public class CustomRestExceptionHandler extends ResponseEntityExceptionHandler {
 
-	@ExceptionHandler(BeanNotFoundException.class)
-	public final ResponseEntity<Object> handleNotFoundException(BeanNotFoundException ex,
-			WebRequest request) {
-		String error = ex.getMessage();
-		ApiError apiError = new ApiError(new Date(),HttpStatus.NOT_FOUND, ex.getLocalizedMessage(), error,request.getDescription(false));
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
-		return new ResponseEntity<Object>(apiError,new HttpHeaders(), apiError.getStatus());
+	@ExceptionHandler(BeanNotFoundException.class)
+	public final ResponseEntity<Object> handleNotFoundException(BeanNotFoundException ex, WebRequest request) {
+		String error = ex.getMessage();
+		ApiError apiError = new ApiError(new Date(), HttpStatus.NOT_FOUND, ex.getLocalizedMessage(), error,
+				request.getDescription(false));
+
+		return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
+	}
+
+	@ExceptionHandler(BadCredentialsException.class)
+	public final ResponseEntity<Object> handleBadCredentialsException(BadCredentialsException ex, WebRequest request) {
+		String error = "Bad Credentials";
+		ApiError apiError = new ApiError(new Date(), HttpStatus.FORBIDDEN, ex.getLocalizedMessage(), error,
+				request.getDescription(false));
+
+		return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
 	}
 
 	@Override
@@ -49,7 +64,8 @@ public class CustomRestExceptionHandler extends ResponseEntityExceptionHandler {
 			errors.add(error.getObjectName() + ": " + error.getDefaultMessage());
 		}
 
-		ApiError apiError = new ApiError(new Date(),HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), errors,request.getDescription(false));
+		ApiError apiError = new ApiError(new Date(), HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), errors,
+				request.getDescription(false));
 		return handleExceptionInternal(ex, apiError, headers, apiError.getStatus(), request);
 	}
 
@@ -59,7 +75,8 @@ public class CustomRestExceptionHandler extends ResponseEntityExceptionHandler {
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
 		String error = ex.getParameterName() + " parameter is missing";
 
-		ApiError apiError = new ApiError(new Date(),HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), error,request.getDescription(false));
+		ApiError apiError = new ApiError(new Date(), HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), error,
+				request.getDescription(false));
 		return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
 	}
 
@@ -72,7 +89,8 @@ public class CustomRestExceptionHandler extends ResponseEntityExceptionHandler {
 					+ violation.getMessage());
 		}
 
-		ApiError apiError = new ApiError(new Date(),HttpStatus.BAD_REQUEST,"could not execute statement:attribute error ", errors,request.getDescription(false));
+		ApiError apiError = new ApiError(new Date(), HttpStatus.BAD_REQUEST,
+				"could not execute statement:attribute error ", errors, request.getDescription(false));
 		return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
 	}
 
@@ -82,26 +100,27 @@ public class CustomRestExceptionHandler extends ResponseEntityExceptionHandler {
 			WebRequest request) {
 		String type = ex.getRequiredType().getCanonicalName().toString();
 		if (type.equals("java.lang.Long"))
-			type="Integer";
-		if(type.equals("java.lang.String"))
-			type="String";
-		String error = ex.getParameter().getParameterName()+ " should be of type "+type ;
-		String errormessage="used a wrong parameter type";
+			type = "Integer";
+		if (type.equals("java.lang.String"))
+			type = "String";
+		String error = ex.getParameter().getParameterName() + " should be of type " + type;
+		String errormessage = "used a wrong parameter type";
 
-		ApiError apiError = new ApiError(new Date(),HttpStatus.BAD_REQUEST, error, errormessage,request.getDescription(false));
+		ApiError apiError = new ApiError(new Date(), HttpStatus.BAD_REQUEST, error, errormessage,
+				request.getDescription(false));
 		return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
 	}
-	
-	//when the data entered is missing a field 
+
+	// when the data entered is missing a field
 	@ExceptionHandler({ DataIntegrityViolationException.class })
-	public ResponseEntity<Object> handleJdbcSQLException(DataIntegrityViolationException ex,
-			WebRequest request) {
-		
-		String error = ex.getCause().getLocalizedMessage();
+	public ResponseEntity<Object> handleJdbcSQLException(DataIntegrityViolationException ex, WebRequest request) {
 
-		ApiError apiError = new ApiError(new Date(),HttpStatus.BAD_REQUEST, error, ex.getRootCause().getLocalizedMessage(),request.getDescription(false));
+		String error = ex.getCause().getLocalizedMessage();
+		ApiError apiError = new ApiError(new Date(), HttpStatus.BAD_REQUEST, error,
+				"the data entered is missing a field", request.getDescription(false));
 		return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
 	}
+
 	//
 	@Override
 	protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex,
@@ -110,13 +129,15 @@ public class CustomRestExceptionHandler extends ResponseEntityExceptionHandler {
 		builder.append(ex.getMethod());
 		builder.append(" method is not supported for this kind of request. Supported methods are ");
 		ex.getSupportedHttpMethods().forEach(t -> builder.append(t + " "));
-		ApiError apiError = new ApiError(new Date(),HttpStatus.METHOD_NOT_ALLOWED, ex.getLocalizedMessage(), builder.toString(),request.getDescription(false));
+		ApiError apiError = new ApiError(new Date(), HttpStatus.METHOD_NOT_ALLOWED, ex.getLocalizedMessage(),
+				builder.toString(), request.getDescription(false));
 		return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
 	}
 
 	@ExceptionHandler({ Exception.class })
 	public ResponseEntity<Object> handleAll(Exception ex, WebRequest request) {
-		ApiError apiError = new ApiError(new Date(),HttpStatus.INTERNAL_SERVER_ERROR, ex.getLocalizedMessage(), "error occurred",request.getDescription(false));
+		ApiError apiError = new ApiError(new Date(), HttpStatus.INTERNAL_SERVER_ERROR, ex.getLocalizedMessage(),
+				"error occurred", request.getDescription(false));
 		return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
 	}
 
@@ -126,7 +147,8 @@ public class CustomRestExceptionHandler extends ResponseEntityExceptionHandler {
 		String error = "Malformed JSON request:the request cannot be fulfilled due to bad syntax";
 		StringBuilder builder = new StringBuilder();
 		builder.append(error);
-		ApiError apiError = new ApiError(new Date(),HttpStatus.BAD_REQUEST,"please check entered information syntax", builder.toString(),request.getDescription(false));
+		ApiError apiError = new ApiError(new Date(), HttpStatus.BAD_REQUEST, "please check entered information syntax",
+				builder.toString(), request.getDescription(false));
 		return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
 	}
 }
